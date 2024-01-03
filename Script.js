5024ecacc74107fff2e2c65ebf37ca4015519c5b
Annonymous321 <93349281+Annonymous321@users.noreply.github.com>
2024-01-03 04:39:23 -0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+function generateDork() {
+  var searchQuery = document.getElementById('searchQuery').value;
+  var googleDork = 'site:stationx.net ' + searchQuery;
+  document.getElementById('result').innerText = "Generated Google Dork: " + googleDork;
+}
