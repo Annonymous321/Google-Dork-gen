9a81d18d22653c4d84c7e4479fc5e7bcc32495c0
Annonymous321 <93349281+Annonymous321@users.noreply.github.com>
2024-01-04 03:37:30 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,31 @@
+const dorkCommands = [
+  "intitle:",
+  "inurl:",
+  "site:",
+  "filetype:",
+  "intext:",
+  "link:",
+  "cache:",
+  "related:",
+  "info:",
+  "map:",
+  "movie:",
+  "weather:",
+  "stocks:",
+  "define:",
+  "book:",
+];
+
+const commandIndex = 0;
+
 document.getElementById('generateButton').addEventListener('click', function() {
   generateDork();
 });
 
 function generateDork() {
-  var searchQuery = document.getElementById('searchQuery').value;
-  var googleDork = 'site:stationx.net ' + searchQuery;
-  document.getElementById('result').innerText = "Generated Google Dork: " + googleDork;
+  const searchQuery = document.getElementById('searchQuery').value;
+  const selectedDork = dorkCommands[commandIndex] + ' "' + searchQuery + '"';
+  document.getElementById('result').innerText = "Generated Google Dork: " + selectedDork;
+
+  commandIndex = (commandIndex + 1) % dorkCommands.length;
 }
