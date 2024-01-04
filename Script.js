d400a44e8b09f58489c138de5568c91b1984327b
Annonymous321 <93349281+Annonymous321@users.noreply.github.com>
2024-01-04 03:44:37 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,7 +16,56 @@ const dorkCommands = [
   "book:",
 ];
 
-const commandIndex = 0;
+let exampleIndex = 0;
+let exampleDisplayInterval;
+
+function displayExample() {
+    let query = examples[exampleIndex];
+    let currentIndex = 0;
+    exampleDisplayInterval = setInterval(() => {
+        if (currentIndex <= query.length) {
+            document.getElementById('searchQuery').setAttribute("placeholder", query.substring(0, currentIndex));
+            currentIndex++;
+        } else {
+            clearInterval(exampleDisplayInterval);
+            if (document.getElementById('searchQuery').value === "") {
+                setTimeout(() => {
+                    eraseExample(query);
+                }, 1000);
+            }
+        }
+    }, 100);
+}
+
+function eraseExample(query) {
+    let currentIndex = query.length;
+    exampleDisplayInterval = setInterval(() => {
+        if (currentIndex >= 0) {
+            document.getElementById('searchQuery').setAttribute("placeholder", query.substring(0, currentIndex));
+            currentIndex--;
+        } else {
+            clearInterval(exampleDisplayInterval);
+            exampleIndex++;
+            if (exampleIndex >= examples.length) {
+                exampleIndex = 0;
+            }
+            setTimeout(() => {
+              displayExample(); // Display the next example
+            }, 1000);
+        }
+    }, 100);
+}
+
+document.getElementById('searchQuery').addEventListener('input', function() {
+    if (this.value === "") {
+        displayExample();
+    } else {
+        if (exampleDisplayInterval) {
+            clearInterval(exampleDisplayInterval);
+        }
+        this.setAttribute("placeholder", "");
+    }
+});
 
 document.getElementById('generateButton').addEventListener('click', function() {
   generateDork();
@@ -24,8 +73,12 @@ document.getElementById('generateButton').addEventListener('click', function() {
 
 function generateDork() {
   const searchQuery = document.getElementById('searchQuery').value;
-  const selectedDork = dorkCommands[commandIndex] + ' "' + searchQuery + '"';
-  document.getElementById('result').innerText = "Generated Google Dork: " + selectedDork;
+  const words = searchQuery.split(" ");
+  let googleDork = "";
+  words.forEach(word => {
+    const randomDork = dorkCommands[Math.floor(Math.random() * dorkCommands.length)];
+    googleDork += randomDork + ' ' + word + ' ';
+  });
 
-  commandIndex = (commandIndex + 1) % dorkCommands.length;
+  document.getElementById('result').innerText = "Generated Google Dork: " + googleDork;
 }
